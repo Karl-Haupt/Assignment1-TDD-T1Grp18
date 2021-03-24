0f41b989388c82c9f2a27a6328edfa7070b9914b
Karl-Haupt <karlhaupt1@gmail.com>
2021-03-24 20:40:16 +0200

    Initial commit || Created project template || Test-Driven Development Assignment 1

---
+++ src/main/java/ac/za/cput/tdd/assignment1/App.java
@@ -0,0 +1,13 @@
+package ac.za.cput.tdd.assignment1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ac/za/cput/tdd/assignment1/AppTest.java
@@ -0,0 +1,12 @@
+package ac.za.cput.tdd.assignment1;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+
+}
